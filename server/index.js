56d74623f78a5b6d67cfbc4556e4e22530e8e79f
Mohamed <midoteraq@gmail.com>
2025-02-02 18:17:32 +0200

    mongodb cloud string

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(session({
     secret: "ARandomStringThatIsHardToGuess12345",
     resave: false,
     saveUninitialized: false,
-    store: MonogoStore.create({mongoUrl: "mongodb://127.0.0.1:27017/bus-system"}),
+    store: MonogoStore.create({mongoUrl: "mongodb+srv://midotareq2:bmNS1j6FLban6kZv@cluster0.ntwma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"}),
     cookie: {
         httpOnly: true,
         maxAge:5000000,
