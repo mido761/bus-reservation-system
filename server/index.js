856d358dd09769cee33cbd7468689f6ccbfe807c
MohamedTareqFarouq <mohamed.farouq@ejust.edu.eg>
2025-02-03 03:42:50 +0200

    cookie CORS

--- server/index.js
+++ server/index.js
@@ -42,6 +42,7 @@ app.use(session({
     store: MonogoStore.create({mongoUrl: "mongodb+srv://midotareq2:bmNS1j6FLban6kZv@cluster0.ntwma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"}),
     cookie: {
         httpOnly: true,
+        sameSite: 'none', // For cross-origin
         secure : true,
         maxAge:5000000,
     }   
