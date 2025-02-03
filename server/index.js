76f6082df40274580ad45ec706270edda52ff212
MohamedTareqFarouq <mohamed.farouq@ejust.edu.eg>
2025-02-03 06:57:45 +0200

    index change

--- server/index.js
+++ server/index.js
@@ -103,6 +103,11 @@ app.post('/login', async  (req,res)=> {
 
         // Set session ID
         req.session.userId = user._id;
+        res.cookie('session_id', req.sessionID, {
+            httpOnly: true,
+            secure: true,
+            maxAge: 600000,
+          });
         console.log(req.session)
         res.status(200).json("Login successful");
     } catch (err) {
