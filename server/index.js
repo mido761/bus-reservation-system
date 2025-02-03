5c7229c68ed8ec56fe106b572fb5f8792de38337
MohamedTareqFarouq <mohamed.farouq@ejust.edu.eg>
2025-02-03 02:39:14 +0200

    auth change

--- server/index.js
+++ server/index.js
@@ -170,7 +170,7 @@ app.get("/auth" , (req,res)=>{
     if(req.session.userId){
         res.status(200).json({authenticated: true,"userId": req.session.userId, "busId":req.session.busId});
     }else{
-        res.status(401).json({authenticated: false})
+        res.status(401).json({authenticated: false}, req)
     }
 })
 
