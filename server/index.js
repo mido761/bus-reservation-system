0b9fc1f417e2f943cade2e8af120118383324946
MohamedTareqFarouq <mohamed.farouq@ejust.edu.eg>
2025-02-03 05:22:34 +0200

    try again

--- server/index.js
+++ server/index.js
@@ -1,212 +1,214 @@
-const express = require("express");
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const cors = require("cors");
-const session = require('express-session');
-const MonogoStore = require("connect-mongo");
-const userModel = require('./models/user');
-const Bus = require('./models/busModel');
-require('dotenv').config();
-const busRoutes = require('./routes/busRoutes');
-const userRouter = require('./routes/userRoutes')
-const SeatSelection = require('./routes/SeatSelection')
-const contactRoutes = require('./routes/contactRoutes');
-
+const express = require("express")
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const cors = require("cors")
+const session = require("express-session")
+const MonogoStore = require("connect-mongo")
+const userModel = require("./models/user")
+const Bus = require("./models/busModel")
+require("dotenv").config()
+const busRoutes = require("./routes/busRoutes")
+const userRouter = require("./routes/userRoutes")
+const SeatSelection = require("./routes/SeatSelection")
+const contactRoutes = require("./routes/contactRoutes")
 
 port = 3001
 
 const app = express()
 app.use(express.json())
-const clientOrigin = 'https://bus-reservation-system-client.vercel.app';
+const clientOrigin = "https://bus-reservation-system-client.vercel.app"
 
-app.use(cors({
-  origin: clientOrigin,         // Allow the frontend origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these methods, including OPTIONS
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow headers like Content-Type and Authorization
-  credentials: true,            // Allow credentials (cookies/tokens) to be included
-}));
+app.use(
+  cors({
+    origin: clientOrigin, // Allow the frontend origin
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow these methods, including OPTIONS
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow headers like Content-Type and Authorization
+    credentials: true, // Allow credentials (cookies/tokens) to be included
+  }),
+)
 
 // Handle OPTIONS preflight request for CORS
-app.options('*', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', clientOrigin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.sendStatus(200); // Respond with 200 for preflight requests
-  });
+app.options("*", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", clientOrigin)
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  res.setHeader("Access-Control-Allow-Credentials", "true")
+  res.sendStatus(200) // Respond with 200 for preflight requests
+})
 
-app.use(session({
+app.use(
+  session({
     secret: "ARandomStringThatIsHardToGuess12345",
     resave: false,
     saveUninitialized: false,
-    store: MonogoStore.create({mongoUrl: "mongodb+srv://midotareq2:bmNS1j6FLban6kZv@cluster0.ntwma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"}),
+    store: MonogoStore.create({
+      mongoUrl:
+        "mongodb+srv://midotareq2:bmNS1j6FLban6kZv@cluster0.ntwma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    }),
     cookie: {
-        httpOnly: true,
-        sameSite: 'None', // For cross-origin
-        secure : true,
-        maxAge:5000000,
-    }   
-}))
+      httpOnly: true,
+      sameSite: "None", // For cross-origin
+      secure: true,
+      maxAge: 5000000,
+    },
+  }),
+)
 
-app.use('/home', (req, res) => {res.send("Server is running")} );
-app.use('/buses', busRoutes);
+app.use((req, res, next) => {
+  console.log("Session ID:", req.sessionID)
+  console.log("Session:", req.session)
+  next()
+})
+
+app.use("/home", (req, res) => {
+  res.send("Server is running")
+})
+app.use("/buses", busRoutes)
 // app.use('/api', bookingRoutes);
-app.use('/seatselection',SeatSelection);
-app.use('/user',userRouter);
-
+app.use("/seatselection", SeatSelection)
+app.use("/user", userRouter)
 
 // Contact routes
-app.use('/contact', contactRoutes);
+app.use("/contact", contactRoutes)
 
-//MongoDB connection 
+//MongoDB connection
 mongoose
-.connect("mongodb+srv://midotareq2:bmNS1j6FLban6kZv@cluster0.ntwma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then( ()=> console.log("MongoDB connected"))
-.catch((err)=> console.error(err));
+  .connect(
+    "mongodb+srv://midotareq2:bmNS1j6FLban6kZv@cluster0.ntwma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err))
 
 //Routes
 // app.get("/api/buses", require('./routes/busRoutes'))
 
+app.post("/login", async (req, res) => {
+  // const {email,password} = req.body;
+  // userModel.findOne({email:email})
+  // .then(user => {
+  //     if (!user){
+  //         return res.status(404).json("This email does not exist");
+  //     }
+  //     const isPasswordValid = bcrypt.compare(password, user.password);
+  //     if(!isPasswordValid){
+  //         return res.status(401).json("The password is incorrect");
+  //     }
+  //     req.session.userId =user._id;
+  //     res.status(200).json("login successful");
+  // })
+  // .catch (err => res.status(500).json("Internal server error"))
+  const { email, password } = req.body
 
-
-
-
-app.post('/login', async  (req,res)=> {
-    // const {email,password} = req.body;
-    // userModel.findOne({email:email})
-    // .then(user => {
-    //     if (!user){
-    //         return res.status(404).json("This email does not exist");
-    //     }
-    //     const isPasswordValid = bcrypt.compare(password, user.password);
-    //     if(!isPasswordValid){
-    //         return res.status(401).json("The password is incorrect");
-    //     }
-    //     req.session.userId =user._id;
-    //     res.status(200).json("login successful");
-    // })
-    // .catch (err => res.status(500).json("Internal server error"))
-    const { email, password } = req.body;
-
-    
-    try {
-        const user = await userModel.findOne({ email });
-        if (!user) {
-            return res.status(404).json("This email does not exist");
-        }
-
-        // Check password validity
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-            return res.status(401).json("The password is incorrect");
-        }
-
-        // Set session ID
-        req.session.userId = user._id;
-        res.cookie('session_id', req.sessionID, {
-            httpOnly: true,
-            secure: true,
-            maxAge: 600000,
-          });
-        console.log(req.session)
-        res.status(200).json("Login successful");
-    } catch (err) {
-        res.status(500).json("Internal server error");
+  try {
+    const user = await userModel.findOne({ email })
+    if (!user) {
+      return res.status(404).json("This email does not exist")
     }
-})
 
-app.post('/register', async (req , res) => {
-    // const {email,password} = req.body;
-    // userModel.findOne({email})
-    // .then(userExist => {
-    //     if(userExist){
-    //         res.json("email already exists")
-    //     }else{ 
-    //         userModel.create(req.body)
-    //         .then(userNew => res.json(userNew))
-    //         .catch(err => res.json(err))
-    //         res.json("succsse")
-    //     }
-    
-    // })
-    // .catch(err => res.json(err))
-
-    const { name,phoneNumber, email, password } = req.body;
-
-    try {
-        const userExist = await userModel.findOne({ email });
-        if (userExist) {
-            return res.json("Email already exists");
-        }
-
-        // Hash the password before saving
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create new user
-        const newUser = await userModel.create({name,phoneNumber,email,password: hashedPassword});
-
-        res.status(201).json(newUser);
-        
-    } catch (err) {
-        res.status(500).json("Internal server error");
+    // Check password validity
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    if (!isPasswordValid) {
+      return res.status(401).json("The password is incorrect")
     }
-})
 
-app.post("/logout",(req, res) => {
-    req.session.destroy(err => {
-        if(err){
-            return res.status(500).json("failed to logout");
-        }
-        res.clearCookie("connect.sid");
-        res.status(200).json("logout successfuly");
+    // Set session ID
+    req.session.userId = user._id
+    res.cookie("session_id", req.sessionID, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 600000,
     })
+    console.log(req.session)
+    res.status(200).json("Login successful")
+  } catch (err) {
+    res.status(500).json("Internal server error")
+  }
 })
 
-app.get("/auth/:busId" , (req,res)=>{
-    const busId = req.params.busId;
-    console.log("Auth Response: ", req.session)
-    req.session.busId = busId
-    if(req.session.userId){
-        res.status(200).json({authenticated: true, "busId":busId});
-    }else{
-        res.status(401).json({authenticated: false})
+app.post("/register", async (req, res) => {
+  // const {email,password} = req.body;
+  // userModel.findOne({email})
+  // .then(userExist => {
+  //     if(userExist){
+  //         res.json("email already exists")
+  //     }else{
+  //         userModel.create(req.body)
+  //         .then(userNew => res.json(userNew))
+  //         .catch(err => res.json(err))
+  //         res.json("succsse")
+  //     }
+
+  // })
+  // .catch(err => res.json(err))
+
+  const { name, phoneNumber, email, password } = req.body
+
+  try {
+    const userExist = await userModel.findOne({ email })
+    if (userExist) {
+      return res.json("Email already exists")
     }
+
+    // Hash the password before saving
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    // Create new user
+    const newUser = await userModel.create({ name, phoneNumber, email, password: hashedPassword })
+
+    res.status(201).json(newUser)
+  } catch (err) {
+    res.status(500).json("Internal server error")
+  }
 })
 
-
-app.get("/auth" , (req,res)=>{
-    if(req.session.userId){
-        res.status(200).json({authenticated: true,"userId": req.session.userId, "busId":req.session.busId});
-    }else{
-        console.log(req.session)
-        res.status(401).json({authenticated: false})
+app.post("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json("failed to logout")
     }
+    res.clearCookie("connect.sid")
+    res.status(200).json("logout successfuly")
+  })
+})
+
+app.get("/auth/:busId", (req, res) => {
+  const busId = req.params.busId
+  console.log("Auth Response: ", req.session)
+  req.session.busId = busId
+  if (req.session.userId) {
+    res.status(200).json({ authenticated: true, busId: busId })
+  } else {
+    res.status(401).json({ authenticated: false })
+  }
+})
+
+app.get("/auth", (req, res) => {
+  if (req.session.userId) {
+    res.status(200).json({ authenticated: true, userId: req.session.userId, busId: req.session.busId })
+  } else {
+    console.log(req.session)
+    res.status(401).json({ authenticated: false })
+  }
 })
 
 // for profile to show the bus that are reserved
-app.post("/payment",async (req,res)=>{
-    // const busId = req.params.busId;
-    const {userId,busId} = req.body;
+app.post("/payment", async (req, res) => {
+  // const busId = req.params.busId;
+  const { userId, busId } = req.body
 
-    const bus = await Bus.findById(busId);
-    const user = await userModel.findById(userId);
+  const bus = await Bus.findById(busId)
+  const user = await userModel.findById(userId)
 
-    res.status(200).json(bus)
-    console.log (bus)
-    if (!bus) {
-      console.log (res.status(404).json({ error: "bus not found" }));
-    }
-    if (!user) {
-        console.log (res.status(404).json({ error: "user not found" }));
-      }
+  res.status(200).json(bus)
+  console.log(bus)
+  if (!bus) {
+    console.log(res.status(404).json({ error: "bus not found" }))
+  }
+  if (!user) {
+    console.log(res.status(404).json({ error: "user not found" }))
+  }
 
-    const updateduser = await userModel.findByIdAndUpdate(
-            userId,
-            {$push:{bookedBuses:busId}},
-            {new : true}
-          );
-    
-   
+  const updateduser = await userModel.findByIdAndUpdate(userId, { $push: { bookedBuses: busId } }, { new: true })
 })
 
 // app.listen(process.env.PORT || 3001 , () =>{
@@ -214,3 +216,4 @@ app.post("/payment",async (req,res)=>{
 // })
 
 module.exports = app
+
