40d608dba6ee9ea41778d096c9ec163a7706d6fe
MohamedTareqFarouq <mohamed.farouq@ejust.edu.eg>
2025-02-03 06:43:26 +0200

    auth change

--- client/src/authent.jsx
+++ client/src/authent.jsx
@@ -9,26 +9,33 @@ const [isLoading, setIsLoading] = useState(true); // Track loading state
 const navigate = useNavigate();
 // Check authentication on component load
 useEffect(() => {
-    const checkAuth = async () => {
-    try {
-        const response = await axios.get("https://bus-reservation-system-server.vercel.app/auth", { withCredentials: true });
-        console.log(response)
-        if (response.data.authenticated) {
-        setIsAuthenticated(true);
-        return response.data.userId
-        } else {
-        setIsAuthenticated(false);
-        navigate("/login");
-        }
-    } catch (error) {
-        console.error("Authentication check failed:", error);
-        setIsAuthenticated(false);
-        navigate("/login");
-    } finally {
-        setIsLoading(false);
-    }
+    const auth = (req, res, next) => {
+        console.log(req.session)
+        if (req?.session?.isAuthenticated) return next(); // User is authenticated, allow them to proceed
+        res.redirect("/login"); // Redirect to login page if not authenticated
     };
-    checkAuth();
+
+    // const checkAuth = async () => {
+    // try {
+    //     const response = await axios.get("https://bus-reservation-system-server.vercel.app/auth", { withCredentials: true });
+    //     console.log(response)
+    //     if (response.data.authenticated) {
+    //     setIsAuthenticated(true);
+    //     return response.data.userId
+    //     } else {
+    //     setIsAuthenticated(false);
+    //     navigate("/login");
+    //     }
+    // } catch (error) {
+    //     console.error("Authentication check failed:", error);
+    //     setIsAuthenticated(false);
+    //     navigate("/login");
+    // } finally {
+    //     setIsLoading(false);
+    // }
+    // };
+    // checkAuth();
+    auth()
 }, [navigate]);
 
 // Show loading state or redirect unauthenticated users
