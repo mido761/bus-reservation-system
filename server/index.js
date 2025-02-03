1583c48bd4e5280f203bd8d8dee2318c2ba8c7f3
MohamedTareqFarouq <mohamed.farouq@ejust.edu.eg>
2025-02-03 06:48:21 +0200

    auth change

--- server/index.js
+++ server/index.js
@@ -102,7 +102,6 @@ app.post('/login', async  (req,res)=> {
         }
 
         // Set session ID
-        console.log(req.session)
         req.session.userId = user._id;
         console.log(req.session)
         res.status(200).json("Login successful");
