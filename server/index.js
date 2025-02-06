bafacbfa098df5b89e1f63dc0fc28a358692dce0
MohamedTareqFarouq <mohamed.farouq@ejust.edu.eg>
2025-02-06 10:29:59 +0200

    Index change

--- server/index.js
+++ server/index.js
@@ -223,7 +223,7 @@ app.post("/payment",async (req,res)=>{
    
 })
 
-if (process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "development"){
     app.use(express.static(path.join(__dirname, '../client/dist')));       
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client/dist/index.html'));
