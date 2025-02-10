52b90d79ab61e21574d0962f85ad43b4a9b62dad
MohamedTareqFarouq <mohamed.farouq@ejust.edu.eg>
2025-02-10 22:18:53 +0200

    try

--- server/index.js
+++ server/index.js
@@ -266,5 +266,8 @@ if (process.env.NODE_ENV === "production") {
   console.log(process.env.NODE_ENV);
 }
 
+app.listen(process.env.PORT || 5000, () => {
+  console.log("sever is running");
+});
 
 module.exports = app;
