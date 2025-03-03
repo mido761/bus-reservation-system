6b8b8767685d8a646a2b0fa8536606c2b536c7bc
MOHAED-ABDALFATTAH <mohamed.abozeid@gmail.com>
2025-03-03 23:45:40 +0200

    naive errors

--- server/index.js
+++ server/index.js
@@ -272,16 +272,4 @@ process.on("SIGINT", () => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-process.on("SIGINT", () => {
-  console.log("Shutting down server...");
-  server.close(() => {
-    console.log("Server closed.");
-    process.exit(0);
-  });
-});
-
-
 module.exports = app;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -66,7 +66,6 @@ router.get("/profile/:userId", async (req, res) => {
 router.post("/profiles", async (req, res) => {
   const { userIds } = req.body;
   const users = await User.find({ _id: { $in: userIds } }, "name phoneNumber bookedBuses.seats checkInStatus"); // Fetch all users at once
-  const users = await User.find({ _id: { $in: userIds } }, "name phoneNumber bookedBuses.seats checkInStatus"); // Fetch all users at once
   res.json(users);
 });
 
