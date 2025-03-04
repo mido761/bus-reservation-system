f750e4670ff5146debae4889e61deaa976fdfa9a
Abdelrahmansa04 <abdelrahman.abdelraouf@ejust.edu.eg>
2025-03-04 04:09:06 +0200

    legend

--- client/src/components/seatSelection/SeatLegend.jsx
+++ client/src/components/seatSelection/SeatLegend.jsx
@@ -5,13 +5,18 @@ const SeatLegend = () => {
   return (
     <div className="seat-legend-container">
       <div className="legend-item">
-        <span className="legend-color booked"></span> Booked Seat
-      </div>
-      <div className="legend-item">
         <span className="legend-color reserved"></span> Temporarily Reserved
       </div>
       <div className="legend-item">
         <span className="legend-color user-seat"></span> Your Seat
+      </div>
+      <div className="legend-item gender-legend">
+        <div>
+          <span className="legend-color girl"></span> Girls
+        </div>
+        <div>
+          <span className="legend-color boy"></span> Boys
+        </div>
       </div>
     </div>
   );
