ca0689766e2c88e802c9f02397cd0cd760817818
MohamedTareqFarouq <mohamed.farouq@ejust.edu.eg>
2025-04-23 19:32:36 +0200

    merge formselection

--- server/routes/FormSelection.js
+++ server/routes/FormSelection.js
@@ -78,34 +78,6 @@ router.post("/:busId", async (req, res) => {
   }
 });
 
-
-
-module.exports = router;
-const express = require("express");
-const router = express.Router();
-const Bus = require("../models/busForm");   
-const User = require("../models/user");
-const Seat = require("../models/seat");
-const innerAuth = require("../controllers/Inner Authorization");
-
-router.get("/:id", async (req, res) => {
-  try {
-    const busId = req.params.id;
-    const bus = await Bus.findById(busId);
-    if (!bus) {
-      return res.status(404).json({ message: "Bus not found" });
-    }
-    res.status(200).json(bus);
-  } catch (err) {
-    console.error("Error fetching bus details:", err);
-    res
-      .status(500)
-      .json({ message: "Error fetching bus details", error: err.message });
-  }
-});
-
-// router.post("/:busId", async (req, res) => {});
-
 router.delete("/:busId", async (req, res) => {
   const busId = req.params.busId;
   const { seatId, userId } = req.body.data;
