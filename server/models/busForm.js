02060bd6021c98b7beee35b418ecd423ac1c25b3
MohamedTareqFarouq <mohamed.farouq@ejust.edu.eg>
2025-04-23 15:37:54 +0200

    Bus form schema added, and form selection

--- server/models/busForm.js
+++ server/models/busForm.js
@@ -1,37 +1,18 @@
 const mongoose = require('mongoose');
 
-const ReservedSeatSchema = new mongoose.Schema({
-    seatNumber: { type: String, required: true },
-    reservedBy: { type: String, required: true },
-    // expiryDate: { 
-    //   type: Date, 
-    //   required: true, 
-    //   default: () => new Date(Date.now() + 10 * 60 * 1000) // Expires in 10 minutes
-    // },
-  });
-
-const BusSchema = new mongoose.Schema({
-    seats: {
-        totalSeats: {type: Number, required: true},
-        bookedSeats: [{type: String,  required: false}],
-        availableSeats: {type: Number, required: false}
-    },
+const BusFormSchema = new mongoose.Schema({
+    bookedSeats: [{type: String,  required: false}],      
     schedule: {type: String, required:true},
     price : {type: Number, required: false},
     location: {
         pickupLocation: {type: String, required: true},
         arrivalLocation: {type: String, required: true},        
     },
-    time: {
-        departureTime: {type: String, required: true},
-        arrivalTime: {type: String, required: true},        
-    },
+    departureTime: {type: String, required: true},
     allowance: {
         cancelTimeAllowance: {type: Number, required: false},
         bookingTimeAllowance: {type: Number, required: false},        
-    },
-    busNumber: {type:Number, required: false, default:"1234"},
-    allowedNumberOfBags: {type: Number, required: false}
+    }
 });
 
-module.exports = mongoose.model('Bus', BusSchema);
+module.exports = mongoose.model('BusForm', BusFormSchema);
