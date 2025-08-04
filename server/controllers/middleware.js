16324262d6bb80c7a06e780bfc802676f32e6bb1
MohamedTareqFarouq <mohamed.farouq@ejust.edu.eg>
2025-08-04 18:21:43 +0300

    auth problem try

--- server/controllers/middleware.js
+++ server/controllers/middleware.js
@@ -19,7 +19,7 @@ const app = express();
 const isAuthenticated = (req, res, next) => {
   if (process.env.NODE_ENV === "development") return next();
   
-  if (req.session && req.session.userId) {
+  if (req.session.userId) {
     return next();
   }
   return res.status(401).json({ message: "unauthenticated: Please log in" });
@@ -41,7 +41,7 @@ const isAuthenticated = (req, res, next) => {
 const isAuthoraized = (req, res, next) => {
   if (process.env.NODE_ENV === "development") return next();
  
-  if (req.session && req.session.userId && req.session.userRole === "admin") {
+  if (req.session.userId && req.session.userRole === "admin") {
     return next();
   }
   return res.status(401).json({ message: "Unauthorized, Access denied" });
