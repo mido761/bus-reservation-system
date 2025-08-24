49a24361c0cbd4d8a8c586d80c7c599a4c0dcdd7
MohamedTareqFarouq <mohamed.farouq@ejust.edu.eg>
2025-08-24 11:56:30 +0300

    HomePage new structure

---
+++ client/src/components/homePage/tripsList/trips.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import LoadingPage from "../loadingPage/loadingPage";
+import LoadingComponent from "../loadingComponent/loadingComponent";
+import LoadingScreen from "../loadingScreen/loadingScreen";
+import Overlay from "../overlayScreen/overlay";
+
+const backEndUrl = import.meta.env.VITE_BACK_END_URL;
+
+const Trips = () => {};
+
+export default Trips;
