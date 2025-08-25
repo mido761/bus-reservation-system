e9f32e5b3b8cec0754679f745f31924757db0ac2
MOHAED-ABDALFATTAH <mohamed.abozeid@ejust.edu.eg>
2025-08-25 19:29:29 +0300

    stops appear in the reserve page

--- client/src/components/Booking/reserve.jsx
+++ client/src/components/Booking/reserve.jsx
@@ -1,9 +1,25 @@
 import { useLocation } from "react-router-dom";
+import { useState,useEffect } from "react";
+import axios from "axios";
 import "./reserve.css";
-
+const backEndUrl = import.meta.env.VITE_BACK_END_URL;
 const Reserve = () => {
   const location = useLocation();
   const { trip, route } = location.state || {};
+  const [stops, setStops] = useState([])
+  useEffect(() => {
+    const fetchStops = async () => {
+      try {
+        const res = await axios.get(`${backEndUrl}/stop/get-stops-route/${route.route_id}`);
+        console.log(res.data.stops)
+        setStops(res.data.stops);
+      } catch (err) {
+        setStops([]);
+      }
+    };
+
+    fetchStops();
+  }, []);
 
   return (
     <div className="reserve-page">
@@ -14,6 +30,15 @@ const Reserve = () => {
           <p><strong>Date:</strong> {trip.date}</p>
           <p><strong>Departure:</strong> {trip.departure_time}</p>
           <p><strong>Arrival:</strong> {trip.arrival_time}</p>
+          <p><strong>stops:</strong></p>
+          <ul>
+            {stops.map((stop) => (
+              <li key={stop.stop_id}>
+                {stop.stop_name} – {stop.location} – {stop.postion} km
+              </li>
+            ))}
+          </ul>
+
 
           <button className="confirm-btn">Confirm Reservation</button>
         </>
