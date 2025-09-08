666aef3c6b273e053d5fea9e7b56064ea75a3642
MohamedTareqFarouq <mohamed.farouq@ejust.edu.eg>
2025-09-08 17:27:34 +0300

    changing session secret and adding sql structure

--- newServer/index.js
+++ newServer/index.js
@@ -89,7 +89,7 @@ const PgSessionStore = pgSession(session);
 app.use(
   session({
     secret:
-      process.env.SESSION_SECRET || "AnotherRandomStringThatIsHardToGuess12345",
+      process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     store: new PgSessionStore({
@@ -244,7 +244,6 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
-
 
 
 // const options = {
