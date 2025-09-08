17e00539805f7df9f35300b6fa115a767c2dd9b2
MohamedTareqFarouq <mohamed.farouq@ejust.edu.eg>
2025-09-08 17:42:59 +0300

    adding db.js

---
+++ newServer/db.js
@@ -0,0 +1,25 @@
+// db.js
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config({path: '../.env'});
+
+// Or use this if any error happened
+// dotenv.config();  
+
+
+const { Pool } = pg;
+const pool = new Pool({
+  connectionString: process.env.NODE_ENV === "production" ? process.env.SUPABASE_URL : process.env.LOCAL_DATABASE_URL,
+//   ssl: { rejectUnauthorized: false },
+});
+
+// Optional: test connection
+pool.connect()
+  .then(client => {
+    console.log("✅ Connected to PostgreSQL");
+    client.release();
+  })
+  .catch(err => console.error("❌ Postgres connection error", err.stack));
+
+export default pool;
