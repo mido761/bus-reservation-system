1a80416203462b3dea8ba6e94a322ec69d032c12
MOHAED-ABDALFATTAH <mohamed.abozeid@ejust.edu.eg>
2025-10-01 00:33:48 +0300

    confirmed the booking

--- newServer/VFcash/confirmPayment.js
+++ newServer/VFcash/confirmPayment.js
@@ -18,6 +18,17 @@ export const confirmVfPayment = async (client, bookingId) => {
       ["confirmed", bookingId]
     );
 
+
+    // Step 1: get trip_id from booking_id
+    const tripRes = await client.query(
+      `SELECT trip_id FROM booking WHERE booking_id = $1`,
+      [bookingId]
+    );
+
+    if (tripRes.rows.length === 0) {
+      throw new Error("Booking not found");
+    }
+
     if (paymentUpdate.rowCount === 0) {
       await client.query("ROLLBACK");
       const err = new Error("Payment already processed or not found");
@@ -25,6 +36,8 @@ export const confirmVfPayment = async (client, bookingId) => {
       throw err;
     }
 
+
+    const tripId = tripRes.rows[0].trip_id;
     //Update booking
     // const bookingId = paymentUpdate.rows[0].booking_id;
     const { rows: bookingRows } = await client.query(
@@ -35,6 +48,46 @@ export const confirmVfPayment = async (client, bookingId) => {
          WHERE booking_id = $3`,
       ["confirmed", 1, bookingId]
     );
+
+    const getBookingTripQuery = `
+    WITH ordered AS (
+      SELECT booking_id,
+            ROW_NUMBER() OVER (ORDER BY booked_at) AS rnk
+      FROM booking
+      WHERE trip_id = $1
+        AND status NOT IN ('cancelled', 'pending')
+    ),
+    batched AS (
+      SELECT booking_id,
+            rnk,
+            FLOOR((rnk - 1)::numeric / $2) AS batch_num
+      FROM ordered
+    ),
+    batch_counts AS (
+      SELECT batch_num, COUNT(*) AS cnt
+      FROM batched
+      GROUP BY batch_num
+    ),
+    full_batches AS (
+      -- only batches that are exactly full (size = min_cap)
+      SELECT batch_num
+      FROM batch_counts
+      WHERE cnt = $2
+    )
+    UPDATE booking b
+    SET status = CASE WHEN fb.batch_num IS NOT NULL THEN 'confirmed' ELSE 'waiting' END
+    FROM batched bt
+    LEFT JOIN full_batches fb ON bt.batch_num = fb.batch_num
+    WHERE b.booking_id = bt.booking_id
+      -- <--- important: do not touch cancelled or pending rows
+      AND b.status NOT IN ('cancelled', 'pending');
+    ` 
+    const getBookingTrip = await client.query(getBookingTripQuery, [tripId,5]);
+    console.log(getBookingTrip)
+
+
+    
+
 
     // Create and send ticket email
     const getTicketQ = `
